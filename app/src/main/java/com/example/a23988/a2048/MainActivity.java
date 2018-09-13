2803e83d401fbbdc22c438595c4cd581af12fb36
acczff <2398817726@qq.com>
2018-09-13 12:09:33 +0800

    animate

--- app/src/main/java/com/example/a23988/a2048/MainActivity.java
+++ app/src/main/java/com/example/a23988/a2048/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.a23988.a2048;
 
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -15,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     final private int HUADONG_MIN_OFFSET = 50;
     private int[][] number_map = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
     private TextView[] tv = new TextView[16];
+    private Button bt;
     private int [] r_id = {R.id.tv1, R.id.tv2, R.id.tv3,R.id.tv4,R.id.tv5,R.id.tv6,R.id.tv7,R.id.tv8,R.id.tv9,R.id.tv10,R.id.tv11,R.id.tv12,R.id.tv13,R.id.tv14,R.id.tv15,R.id.tv16};
     private int [] background_color = new int[11];
     private float down_x = 0;
@@ -26,16 +32,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = findViewById(R.id.bt);
         for(int i = 0;i < r_id.length;i++){
             tv[i] = findViewById(r_id[i]);
         }
        // numberchange(number_map,4);
-        random(number_map);
-        random(number_map);
+        random(number_map, new OnChange() {
+            @Override
+            public void onchusheng(int index) {
+                animation(index,0);
+            }
+
+            @Override
+            public void onhebing(int index) {
+
+            }
+        });
+        random(number_map, new OnChange() {
+            @Override
+            public void onchusheng(int index) {
+                animation(index,0);
+            }
+
+            @Override
+            public void onhebing(int index) {
+
+            }
+        });
         for (int i = 0;i < background_color.length;i++){
             background_color[i] = Color.argb(255,238,220 - i*17,0);
         }
         set_texts_color();
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int i = 0;i < number_map.length;i++){
+                    for(int ii = 0;ii < number_map[i].length;ii++){
+                        number_map[i][ii] = 0;
+                    }
+                }
+                random(number_map, new OnChange() {
+                    @Override
+                    public void onchusheng(int index) {
+                        animation(index,0);
+                    }
+
+                    @Override
+                    public void onhebing(int index) {
+
+                    }
+                });
+                random(number_map, new OnChange() {
+                    @Override
+                    public void onchusheng(int index) {
+                        animation(index,0);
+                    }
+
+                    @Override
+                    public void onhebing(int index) {
+
+                    }
+                });
+                set_texts_color();
+            }
+        });
     }
 
     @Override
@@ -49,48 +109,201 @@ public class MainActivity extends AppCompatActivity {
             up_y = event.getY();
             float offsetX = Math.abs(up_x - down_x);
             float offsetY = Math.abs(up_y - down_y);
+            int [][] a = new int[number_map.length][number_map[0].length];
             if (offsetX > offsetY) {
                 if (down_x - up_x > HUADONG_MIN_OFFSET) {
                     //左滑
                     //ss();
-                    numberchange(number_map,1);
-                    random(number_map);
-                    set_texts_color();
+                    boolean boo = true;
+                    for(int i = 0;i < number_map.length;i++){
+                        for(int ii = 0;ii < number_map[i].length;ii++){
+                            a[i][ii] = number_map[i][ii];
+                        }
+                    }
+                    numberchange(number_map, 1, new OnChange() {
+                        @Override
+                        public void onchusheng(int index) {
+
+                        }
+
+                        @Override
+                        public void onhebing(int index) {
+                            animation(index,1);
+                        }
+                    });
+                    gll:for(int i = 0;i < number_map.length;i++){
+                        for(int ii = 0;ii < number_map[i].length;ii++){
+                            if(a[i][ii] != number_map[i][ii]){
+                                boo = false;
+                                break gll;
+                            }
+                        }
+                    }
+                    if(boo){
+                        set_texts_color();
+                    }else {
+                        random(number_map, new OnChange() {
+                            @Override
+                            public void onchusheng(int index) {
+                                animation(index,0);
+                            }
+
+                            @Override
+                            public void onhebing(int index) {
+
+                            }
+                        });
+                        set_texts_color();
+                    }
                     //ss();
-                    Log.d("aaaaa", "onTouchEvent: 左滑");
+                    //Log.d("aaaaa", "onTouchEvent: 左滑");
                 } else if (up_x - down_x > HUADONG_MIN_OFFSET) {
                     //右划
                     //ss();
-                    numberchange(number_map,2);
-                    random(number_map);
-                    set_texts_color();
+                    boolean boo = true;
+                    for(int i = 0;i < number_map.length;i++){
+                        for(int ii = 0;ii < number_map[i].length;ii++){
+                            a[i][ii] = number_map[i][ii];
+                        }
+                    }
+                    numberchange(number_map, 2, new OnChange() {
+                        @Override
+                        public void onchusheng(int index) {
+
+                        }
+
+                        @Override
+                        public void onhebing(int index) {
+                            animation(index,1);
+                        }
+                    });
+                    gll:for(int i = 0;i < number_map.length;i++){
+                        for(int ii = 0;ii < number_map[i].length;ii++){
+                            if(a[i][ii] != number_map[i][ii]){
+                                boo = false;
+                                break gll;
+                            }
+                        }
+                    }
+                    if(boo){
+                        set_texts_color();
+                    }else {
+                        random(number_map, new OnChange() {
+                            @Override
+                            public void onchusheng(int index) {
+                                animation(index,0);
+                            }
+
+                            @Override
+                            public void onhebing(int index) {
+
+                            }
+                        });
+                        set_texts_color();
+                    }
                     //ss();
-                    Log.d("aaaaa", "onTouchEvent: 右滑");
+                    //Log.d("aaaaa", "onTouchEvent: 右滑");
                 }
             } else {
                 if (down_y - up_y > HUADONG_MIN_OFFSET) {
                     //上滑
                     //ss();
-                    numberchange(number_map,3);
-                    random(number_map);
-                    set_texts_color();
+                    boolean boo = true;
+                    for(int i = 0;i < number_map.length;i++){
+                        for(int ii = 0;ii < number_map[i].length;ii++){
+                            a[i][ii] = number_map[i][ii];
+                        }
+                    }
+                    numberchange(number_map, 3, new OnChange() {
+                        @Override
+                        public void onchusheng(int index) {
+
+                        }
+
+                        @Override
+                        public void onhebing(int index) {
+                            animation(index,1);
+                        }
+                    });
+                    gll:for(int i = 0;i < number_map.length;i++){
+                        for(int ii = 0;ii < number_map[i].length;ii++){
+                            if(a[i][ii] != number_map[i][ii]){
+                                boo = false;
+                                break gll;
+                            }
+                        }
+                    }
+                    if(boo){
+                        set_texts_color();
+                    }else {
+                        random(number_map, new OnChange() {
+                            @Override
+                            public void onchusheng(int index) {
+                                animation(index,0);
+                            }
+
+                            @Override
+                            public void onhebing(int index) {
+
+                            }
+                        });
+                        set_texts_color();
+                    }
                     //ss();
-                    Log.d("aaaaa", "onTouchEvent: 上滑");
+                    //Log.d("aaaaa", "onTouchEvent: 上滑");
                 } else if (up_y - down_y > HUADONG_MIN_OFFSET) {
                     //下滑
                     //ss();
-                    numberchange(number_map,4);
-                    random(number_map);
-                    set_texts_color();
+                    boolean boo = true;
+                    for(int i = 0;i < number_map.length;i++){
+                        for(int ii = 0;ii < number_map[i].length;ii++){
+                            a[i][ii] = number_map[i][ii];
+                        }
+                    }
+                    numberchange(number_map, 4, new OnChange() {
+                        @Override
+                        public void onchusheng(int index) {
+
+                        }
+
+                        @Override
+                        public void onhebing(int index) {
+                            animation(index,1);
+                        }
+                    });
+                    gll:for(int i = 0;i < number_map.length;i++){
+                        for(int ii = 0;ii < number_map[i].length;ii++){
+                            if(a[i][ii] != number_map[i][ii]){
+                                boo = false;
+                                break gll;
+                            }
+                        }
+                    }
+                    if(boo){
+                        set_texts_color();
+                    }else {
+                        random(number_map, new OnChange() {
+                            @Override
+                            public void onchusheng(int index) {
+                                animation(index,0);
+                            }
+
+                            @Override
+                            public void onhebing(int index) {
+
+                            }
+                        });
+                        set_texts_color();
+                    }
                     //ss();
-                    Log.d("aaaaa", "onTouchEvent: 下滑");
+                    //Log.d("aaaaa", "onTouchEvent: 下滑");
                 }
             }
         }
         return false;
     }
 
-    public void numberchange(int[][] number_map,int huadong_state){
+    public void numberchange(int[][] number_map,int huadong_state,OnChange onChange){
         int n = 0;
         int i = 0;
         switch (huadong_state){
@@ -113,6 +326,7 @@ public class MainActivity extends AppCompatActivity {
                     for(int o = 0;o < number_map[m].length - 1;o++){
                         if(number_map[m][o] == number_map[m][o + 1]){
                             number_map[m][o] *= 2;
+                            onChange.onhebing(m*4+o);
                             if(o == number_map[m].length - 1){
                                 number_map[m][o + 1] = 0;
                             }else {
@@ -123,6 +337,8 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     }
+
+
                 }
 
                 break;
@@ -146,6 +362,7 @@ public class MainActivity extends AppCompatActivity {
                     //合并并重新排列
                     for(int o = number_map[m].length - 1;o > 0;o--){
                         if(number_map[m][o] == number_map[m][o - 1]){
+                            onChange.onhebing(m*4+o);
                             number_map[m][o] *= 2;
                             if(o - 1 == number_map[m].length){
                                 number_map[m][o - 1] = 0;
@@ -177,6 +394,7 @@ public class MainActivity extends AppCompatActivity {
                     //合并并重新排列
                     for(int o = 0;o < number_map.length - 1;o++){
                         if(number_map[o][m] == number_map[o + 1][m]){
+                            onChange.onhebing(o*4+m);
                             number_map[o][m] *= 2;
                             if(o == number_map.length - 1){
                                 number_map[o + 1][m] = 0;
@@ -211,6 +429,7 @@ public class MainActivity extends AppCompatActivity {
                     //合并并重新排列
                     for(int o = number_map.length - 1;o > 0;o--){
                         if(number_map[o][m] == number_map[o - 1][m]){
+                            onChange.onhebing(o*4+m);
                             number_map[o][m] *= 2;
                             if(o - 1 == 0){
                                 number_map[o - 1][m] = 0;
@@ -228,7 +447,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void random(int[][] number_map){
+    public void random(int[][] number_map,OnChange onChange){
         int [] a = new int[2];
         Random ra = new Random();
         for(int m = 0;m <number_map.length;m++){
@@ -240,6 +459,7 @@ public class MainActivity extends AppCompatActivity {
                     }while (number_map[a[0]][a[1]] != 0);
                     int [] b = {2,4};
                     number_map[a[0]][a[1]] = b[ra.nextInt(2)];
+                    onChange.onchusheng(a[0]*4+a[1]);
                     return;
                 }
             }
@@ -254,13 +474,49 @@ public class MainActivity extends AppCompatActivity {
                     tv[m*4+n].setText(number_map[m][n]+"");
                     int  d = (int) (Math.log(number_map[m][n]) / Math.log(2));
                     Log.d("ddd", ""+ d);
-                    tv[m*4+n].setBackgroundColor(background_color[d - 1]);
+                    GradientDrawable drawable = (GradientDrawable) tv[m*4+n].getBackground();
+                    drawable.setColor(background_color[d - 1]);
                 }else {
-                    tv[m*4+n].setText("");
-                    tv[m*4+n].setBackgroundColor(0xeeeeeeee);
+                    tv[m*4+n].setText(" ");
+                    GradientDrawable drawable = (GradientDrawable) tv[m*4+n].getBackground();
+                    drawable.setColor(0xeeeeeeee);
                 }
 
             }
+        }
+    }
+
+    public void animation(final int n,int i){
+        final ScaleAnimation animator;
+        switch (i){
+            case 0:
+                animator = new ScaleAnimation(0.05f,1.0f,0.05f,1.0f, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+                animator.setDuration(200);
+                tv[n].startAnimation(animator);
+                break;
+            case 1:
+                animator = new ScaleAnimation(1.0f,1.1f,1.0f,1.1f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+                animator.setDuration(200);
+                animator.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        animation =  new ScaleAnimation(1.1f,1.0f,1.1f,1.0f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+                        animation.setDuration(200);
+                        tv[n].startAnimation(animation);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                tv[n].startAnimation(animator);
+                break;
         }
     }
 
@@ -275,4 +531,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d("aaaaa", sb.toString());
     }
 
+
+}
+interface OnChange{
+    void onchusheng(int index);
+
+    void onhebing(int index);
 }
